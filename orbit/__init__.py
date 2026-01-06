3990da71619123ac1777e1f061c2679760f29fc3
Aiden <acdphc@qq.com>
2026-01-06 23:35:43 +0800

    Update __init__.py

--- orbit/__init__.py
+++ orbit/__init__.py
@@ -1,9 +1,10 @@
 from orbit.engine import Engine
 
-import orbit.model  as model
-import orbit.utils  as utils
-import orbit.optim  as optim
-import orbit.plugin as plugin
+import orbit.model   as model
+import orbit.utils   as utils
+import orbit.optim   as optim
+import orbit.plugin  as plugin
+import orbit.dataset as dataset
 
 seed_info = None
 
